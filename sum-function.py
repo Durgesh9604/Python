40f2ba8d7f868a02868e4e6dc1b21beae797efc1
DURGESH SINGH <durgesh20336@gmail.com>
2025-10-20 15:06:23 +0530

    Enhance arthmatic operation function with additional operations
    Added subtraction and multiplication operations to the sum function.

--- sum-function.py
+++ sum-function.py
@@ -1,9 +1,14 @@
 def sum():
-
+    
     a=int(input("enter the number a:"))
 
     b=int(input("enter the number  b:"))
     
-    print("sum of the numbers:",a+b);
+    print("sum of the numbers:",a+b)
 
+    print("substraction of numbers:",a-b)
+
+    print("multiplication of number:",a*b)
+    
 sum()
+
